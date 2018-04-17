24153978211d2d6fd063d56ca169d44a7ffa1a52
lebirdalabig <von.arimbay598@gmail.com>
2018-04-17 16:26:24 +0800

    Updated DB with users

--- webappdb.sql
+++ webappdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 13, 2018 at 01:22 AM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.8
+-- Generation Time: Apr 17, 2018 at 10:19 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -62,26 +62,26 @@ CREATE TABLE `cinema` (
 --
 
 INSERT INTO `cinema` (`cinema_id`, `cinema_number`, `cinema_type`, `building_id`) VALUES
-(21, 'Cinema 1', 'Digital', 2),
-(22, 'Cinema 2', 'Digital', 2),
-(23, 'Cinema 3', 'Digital', 2),
-(24, 'IMAX', 'IMAX', 2),
-(25, 'Cinema 5', 'Digital', 2),
-(26, 'Cinema 6', 'Digital', 2),
-(27, 'Cinema 7', 'Digital', 2),
-(28, 'Cinema 8', 'Digital', 2),
-(29, 'Digital Cinema 1', 'Digital', 3),
-(30, 'Digital Cinema 2', 'Digital', 3),
-(31, 'Digital Cinema 3', 'Digital', 3),
-(32, 'Digital Cinema 4', 'Digital', 3),
-(33, 'Director\'s Club 1', 'Digital', 3),
-(34, 'Director\'s Club 2', 'Digital', 3),
-(35, 'Large Screen Cinema', 'Digital', 3),
-(36, 'Center Stage', 'Digital', 3),
-(37, 'Cinema 1', 'Digital', 4),
-(38, 'Cinema 2', 'Digital', 4),
-(39, 'Cinema 3', 'Digital', 4),
-(40, 'Cinema 4', 'Digital', 4);
+(1, 'Cinema 1', 'Digital', 2),
+(2, 'Cinema 2', 'Digital', 2),
+(3, 'IMAX', 'IMAX', 2),
+(4, 'Cinema 5', 'Digital', 2),
+(5, 'Cinema 6', 'Digital', 2),
+(6, 'Cinema 7', 'Digital', 2),
+(7, 'Cinema 8', 'Digital', 2),
+(8, 'Digital Cinema 1', 'Digital', 3),
+(9, 'Digital Cinema 2', 'Digital', 3),
+(10, 'Digital Cinema 3', 'Digital', 3),
+(11, 'Digital Cinema 4', 'Digital', 3),
+(12, 'Director\'s Club 1', 'Digital', 3),
+(13, 'Director\'s Club 2', 'Digital', 3),
+(14, 'Large Screen Cinema', 'Digital', 3),
+(15, 'Center Stage', 'Digital', 3),
+(16, 'Cinema 1', 'Digital', 4),
+(17, 'Cinema 2', 'Digital', 4),
+(18, 'Cinema 3', 'Digital', 4),
+(19, 'Cinema 4', 'Digital', 4),
+(20, 'Cinema 3', 'Digital', 2);
 
 -- --------------------------------------------------------
 
@@ -94,7 +94,7 @@ CREATE TABLE `movie` (
   `movie_title` varchar(100) NOT NULL,
   `movie_desc` varchar(500) NOT NULL,
   `movie_year` year(4) NOT NULL,
-  `movie_rating` varchar(5) NOT NULL,
+  `movie_rating` enum('G','PG','PG-13','R','NC-17') NOT NULL,
   `movie_poster` varchar(10000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -103,16 +103,9 @@ CREATE TABLE `movie` (
 --
 
 INSERT INTO `movie` (`movie_id`, `movie_title`, `movie_desc`, `movie_year`, `movie_rating`, `movie_poster`) VALUES
-(11, 'Black Panther', 'Lorem ipsum', 2017, 'PG13', 'images/m24.jpg'),
-(12, 'Kingsman: The Golden Circle', 'Lorem ipsum', 2017, 'R', 'images/m2.jpg'),
-(13, 'Spider-Man: Homecoming', 'Lorem ipsum', 2017, 'PG13', 'images/m3.jpg'),
-(14, 'Wonder Woman', 'Lorem ipsum', 2017, 'PG13', 'images/m4.jpg'),
-(15, 'Transformers: The Last Knight ', 'Lorem ipsum', 2017, 'PG13', 'images/m1.jpg'),
-(16, 'Beauty and the Beast ', 'Lorem ipsum', 2017, 'PG13', 'images/m1.jpg'),
-(17, 'Pirates of the Caribbean: Dead Men Tell No Tales', 'Lorem ipsum', 2017, 'PG13', 'images/m1.jpg'),
-(18, 'The LEGO Ninjago Movie', 'Lorem ipsum', 2017, 'PG13', 'images/m1.jpg'),
-(19, 'Guardians of the Galaxy Vol. 2', 'Lorem ipsum', 2017, 'PG13', 'images/m1.jpg'),
-(20, 'Annabelle: Creation ', 'Lorem ipsum', 2017, 'PG13', 'images/m1.jpg');
+(1, 'Moana', 'An adventurous teenager sails out on a daring mission to save her people. During her journey, Moana meets the once-mighty demigod Maui, who guides her in her quest to become a master way-finder. Together they sail across the open ocean on an action-packed voyage, encountering enormous monsters and impossible odds. Along the way, Moana fulfills the ancient quest of her ancestors and discovers the one thing she always sought: her own identity.', 2017, 'PG', 'images/c1.jpg'),
+(7, 'Mike and Dave Need Wedding Dates', 'Mike and Dave Stangle are young, adventurous, fun-loving brothers who tend to get out of control at family gatherings. When their sister Jeanie reveals her Hawaiian wedding plans, the rest of the clan insists that they both bring respectable dates. After placing an ad on Craigslist, the siblings decide to pick Tatiana and Alice, two charming and seemingly normal women. Once they arrive on the island, however, Mike and Dave realize that their companions are ready to get wild and party hard.', 2017, 'R', 'images/c7.jpg'),
+(9, 'Barbershop: The Next Cut', 'To survive harsh economic times, Calvin and Angie have merged the barbershop and beauty salon into one business. The days of male bonding are gone as Eddie and the crew must now contend with sassy female co-workers and spirited clientele. As the battle of the sexes rages on, a different kind of conflict has taken over Chicago. Crime and gangs are on the rise, leaving Calvin worried about the fate of his son. Together, the friends come up with a bold plan to take back their beloved neighborhood.', 2017, 'PG-13', 'images/c9.jpg');
 
 -- --------------------------------------------------------
 
@@ -127,6 +120,13 @@ CREATE TABLE `screening` (
   `movie_id` int(11) NOT NULL,
   `cinema_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `screening`
+--
+
+INSERT INTO `screening` (`screening_id`, `screening_date`, `screening_sched`, `movie_id`, `cinema_id`) VALUES
+(1, '2018-03-15', '10:30-12:00', 1, 8);
 
 -- --------------------------------------------------------
 
@@ -166,6 +166,8 @@ CREATE TABLE `transaction` (
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
   `user_fname` varchar(100) NOT NULL,
   `user_lname` varchar(100) NOT NULL,
   `user_bod` date NOT NULL,
@@ -183,8 +185,12 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `user_fname`, `user_lname`, `user_bod`, `user_gender`, `user_civil`, `user_address`, `user_province`, `user_city`, `user_mobile`, `user_email`, `user_type`) VALUES
-(21, 'Andrew', 'Edioma', '1998-07-04', 'Male', 'Single', 'Deca Homes', 'Cebu', 'Mandaue City', '09330495195', 'andrewedioma@gmail.com', 'Admin');
+INSERT INTO `user` (`user_id`, `username`, `password`, `user_fname`, `user_lname`, `user_bod`, `user_gender`, `user_civil`, `user_address`, `user_province`, `user_city`, `user_mobile`, `user_email`, `user_type`) VALUES
+(1, 'mrmonday', 'monday', 'Andrew', 'Edioma', '1998-07-04', 'Male', 'Single', 'Deca Homes', 'Cebu', 'Mandaue City', '09330495195', 'andrewedioma@gmail.com', 'Admin'),
+(2, 'lebigalabird', 'bird', 'Francis Von Dominic', 'Arimbay', '1998-05-09', 'Female', 'Married', 'Cebu', 'Cebu', 'Cebu', '09494814799', 'von.arimbay598@gmail.com', 'Customer'),
+(3, 'faw', 'faw', 'fawfwa', 'fwaf', '0000-00-00', '', '', '', '', '', '1232', 'faw@faw', 'Customer'),
+(4, 'Joe', 'Joe', 'Joe', 'Joe', '0000-00-00', '', '', '', '', '', '12321', 'Joe@gmail.com', 'Customer'),
+(5, 'BUzz', 'buzz', 'Josh', 'Buzz', '0000-00-00', '', '', '', '', '', '01982365178', 'BUzz@gmail.com', 'Customer');
 
 --
 -- Indexes for dumped tables
@@ -247,36 +253,43 @@ ALTER TABLE `user`
 --
 ALTER TABLE `building`
   MODIFY `building_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `cinema`
 --
 ALTER TABLE `cinema`
-  MODIFY `cinema_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `cinema_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
 --
 -- AUTO_INCREMENT for table `movie`
 --
 ALTER TABLE `movie`
-  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `movie_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
 --
 -- AUTO_INCREMENT for table `screening`
 --
 ALTER TABLE `screening`
-  MODIFY `screening_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `screening_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `seat`
 --
 ALTER TABLE `seat`
   MODIFY `seat_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
   MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- Constraints for dumped tables
 --
